37f95d5a5d0b0a12cb722fa6d2220eb74c0e43fa
Wissal Loutfi <147111691+loutfiwissal@users.noreply.github.com>
2024-06-07 00:26:21 +0100

    Add files via upload

---
+++ permuter.js
@@ -0,0 +1,18 @@
+function permuter(){
+    // event.preventDefault();
+    var nb1= document.getElementById("nb1").value;
+    var nb2= document.getElementById("nb2").value;
+    var valeur = nb1;
+    nb1 = nb2 ;
+    nb2 = valeur ;
+    document.getElementById("nb1").value = nb1 ;
+    document.getElementById("nb2").value = nb2 ;
+    var x = 7
+
+
+
+    var x=10
+    var X =60
+    
+
+}
